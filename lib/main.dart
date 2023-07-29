4ec4aeab64661bcde65b75ab39a63bef0fe94a2c
Aakriti Chandra <129523764+Aakritic28@users.noreply.github.com>
2023-07-29 11:31:57 +0530

    week3_assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,131 +1,430 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'category.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'login_page.dart';
 
-void main() => runApp(MyApp());
+class ExpenseDatabase {
+  final DatabaseReference _databaseReference =
+  FirebaseDatabase.instance.ref();
+  final String _userId;
 
-class MyApp extends StatelessWidget {
+  ExpenseDatabase(this._userId);
+
+  void addExpense(Expense expense) {
+    final newExpenseRef = _databaseReference
+        .child('users')
+        .child(_userId)
+        .child('expenses')
+        .push();
+    newExpenseRef.set({
+      'category': expense.category,
+      'amount': expense.amount,
+    });
+  }
+
+  void removeExpense(String expenseKey) {
+    _databaseReference
+        .child('users')
+        .child(_userId)
+        .child('expenses')
+        .child(expenseKey)
+        .remove();
+  }
+}
+
+
+class ExpenseProvider with ChangeNotifier {
+  double _total = 0;
+  List<Expense> _expenses = [];
+
+  double get total => _total;
+  List<Expense> get expenses => _expenses;
+
+  final ExpenseDatabase _expenseDatabase;
+
+  ExpenseProvider(this._expenseDatabase);
+
+  void addExpense(Expense expense) {
+    _expenses.add(expense);
+    _total += expense.amount;
+    _expenseDatabase.addExpense(expense); // Update Firebase Database
+    notifyListeners();
+  }
+
+  void removeExpense(Expense expense, String expenseKey) {
+    _expenses.remove(expense);
+    _total -= expense.amount;
+    _expenseDatabase.removeExpense(expenseKey); // Update Firebase Database
+    notifyListeners();
+  }
+}
+
+class Expense {
+  final String category;
+  final double amount;
+
+  Expense({required this.category, required this.amount});
+}
+
+class BudgetTrackerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomePage(),
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, AsyncSnapshot<User?> snapshot) {
+        if (snapshot.hasData && snapshot.data != null) {
+          // Get the user ID
+          final userId = snapshot.data!.uid;
+
+          // Create an instance of ExpenseDatabase
+          final expenseDatabase = ExpenseDatabase(userId);
+
+          return ChangeNotifierProvider(
+            create: (_) => ExpenseProvider(expenseDatabase),
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Budget Tracker',
+              home: HomeScreen(),
+              routes: {
+                '/expense': (context) => ExpenseScreen(),
+              },
+            ),
+          );
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Budget Tracker',
+          home: LoginScreen(),
+        );
+      },
     );
   }
 }
 
-// HomePage - Updated
 
-// HomePage - Updated as StatefulWidget
-
-class HomePage extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    final expenseProvider = Provider.of<ExpenseProvider>(context);
+    final total = expenseProvider.total;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Budget Tracker Home Page',
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Montserrat',
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.black54,
+        centerTitle: true,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: const RadialGradient(
+            colors: [Colors.cyanAccent, Colors.lightBlue],
+            radius: 1,
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(
+                Icons.account_circle_sharp,
+                size: 250,
+              ),
+              Text(
+                'Welcome Back User!',
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontFamily: 'Lumanosimo',
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  title: Row(
+                    children: [
+                      Text(
+                        'Balance: ',
+                        style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                      Consumer<ExpenseProvider>(
+                        builder: (context, expenseProvider, _) => Text(
+                          expenseProvider.total.toString(),
+                          style: TextStyle(
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+
+                    ],
+                  ),
+                  trailing: Icon(Icons.arrow_forward),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/expense');
+                  },
+                ),
+              ),
+
+              ElevatedButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();},
+                child: const Text(
+                    'Logout'
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage> {
-  // Dummy data for demonstration
-  final List<Category> categories = [
-    Category(name: 'Groceries', amount: -100),
-    Category(name: 'Salary', amount: 50000),
-    Category(name: 'Water Bill', amount: -500),
-    Category(name: 'Electricity', amount: -8000),
-  ];
+class ExpenseScreen extends StatelessWidget {
+  final TextEditingController categoryController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
 
-  double totalExpenses = 0.0;
 
-  void updateTotalExpenses() {
-    totalExpenses = categories.fold(0.0, (sum, category) => sum + category.amount);
+  void addExpense(BuildContext context) {
+    final category = categoryController.text.trim();
+    double amount = double.tryParse(amountController.text.trim()) ?? 0.0;
+
+    if (category.isNotEmpty && amount != 0) {
+      final expense = Expense(category: category, amount: amount);
+      Provider.of<ExpenseProvider>(context, listen: false).addExpense(expense);
+
+      categoryController.clear();
+      amountController.clear();
+    }
   }
 
-  @override
-  void initState() {
-    super.initState();
-    updateTotalExpenses(); // Calculate total expenses initially
+  void deleteExpense(BuildContext context, Expense expense, String expenseKey) {
+    Provider.of<ExpenseProvider>(context, listen: false).removeExpense(expense, expenseKey); // Pass both the expense and the key
+  }
+
+  Future<void> openDialog(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        //contentPadding: EdgeInsets.all(20.0),
+        title: Center(
+            child:Text('New Entry',
+                style:TextStyle(
+                    color: Colors.pink,
+                    fontSize: 28,
+                    fontFamily: 'Lumanosimo'
+                ))),
+
+        content: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: categoryController,
+                autofocus: true,
+                decoration: InputDecoration(
+                  labelText: 'Category',
+                  labelStyle: TextStyle(
+                      color: Colors.purple,
+                      fontSize: 20,
+                      fontStyle: FontStyle.italic,
+                      fontFamily: 'Montserrat'
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black,width:3.0),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+
+                  ),
+                  filled: true,
+                  fillColor: Colors.white30,
+                ),
+              ),
+              TextField(
+                controller: amountController,
+                decoration: InputDecoration(
+                  labelText: 'Amount',
+                  labelStyle: TextStyle(
+                      color: Colors.purple,
+                      fontSize: 18,
+                      fontStyle: FontStyle.italic,
+                      fontFamily: 'Montserrat'
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black,width:3.0),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white30,
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  signed: true,
+                  decimal: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          FloatingActionButton(
+            onPressed: () {
+              addExpense(context); // Call the addExpense method with the context
+              Navigator.pop(context); // Close the dialog after adding the expense
+            },
+            child: Icon(Icons.done_outline_rounded),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    final expenseProvider = Provider.of<ExpenseProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'Expense Tracker',
+          'Expense',
           style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Montserrat',
             fontWeight: FontWeight.bold,
-            fontSize: 30,
-            fontFamily: 'Trajan Pro',
           ),
         ),
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.black54,
+        centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 20),
-            CircleAvatar(
-              radius: 40,
-              backgroundImage: AssetImage('assets/login.jpg'),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'USER LOGIN',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-
-              ),
-            ),
-            SizedBox(height: 50),
-            Container(
-              width: 350,
-              height: 80,
-              color: Colors.purple,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Total Expenses',
+      body: Container(
+        color: Colors.cyan[200],
+        padding: EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            children: [
+              Card(
+                child: ListTile(
+                  title: Text(
+                    'Balance: ',
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
+                      fontFamily: 'Montserrat',
                       fontWeight: FontWeight.bold,
-
+                      fontSize: 18,
                     ),
                   ),
-                  Text(
-                    'Rs ${totalExpenses.toStringAsFixed(2)}',
-                    style: TextStyle(color: Colors.white, fontSize: 24),
+                  trailing: Consumer<ExpenseProvider>(
+                    builder: (context, expenseProvider, _) => Text(
+                      expenseProvider.total.toString(),
+                      style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
                   ),
-                ],
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: expenseProvider.expenses.length,
+                  itemBuilder: (context, index) {
+                    final expense = expenseProvider.expenses[index];
+                    final expenseKey = ''; // Get the expense key here from your Firebase Database
+                    return ExpenseCard(
+                      expense: expense,
+                      onDelete: () => deleteExpense(context, expense, expenseKey), // Pass both the expense and the key
+                    );
+                  },
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          openDialog(context); // Call the openDialog method with the context
+        },
+        child: Icon(Icons.add),
+      ),
+
+    );
+  }
+}
+
+class ExpenseCard extends StatelessWidget {
+  final Expense expense;
+  final VoidCallback onDelete;
+
+  const ExpenseCard({
+    required this.expense,
+    required this.onDelete,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          expense.category,
+          style: TextStyle(
+            color: Colors.pink,
+            fontFamily: 'Lugrasimo',
+            fontSize: 20,
+          ),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              '${expense.amount}',
+              style: TextStyle(
+                color: Colors.purpleAccent,
+                fontFamily: 'Lugrasimo',
+                fontSize: 20,
               ),
             ),
-            SizedBox(height: 20),
             IconButton(
-              icon: Icon(Icons.keyboard_arrow_down, size: 36),
-              onPressed: () async {
-                final newCategory = await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => CategoryPage(
-                      categories,
-                      onCategoryAdded: (category) {
-                        setState(() {
-                          categories.add(category);
-                          updateTotalExpenses(); // Update total expenses
-                        });
-                      },
-                    ),
-                  ),
-                );
-
-                // Handle the newCategory if needed (optional)
-                if (newCategory != null) {
-                  // You can perform additional actions with the newCategory here if needed.
-                }
-              },
+              icon: Icon(Icons.delete),
+              onPressed: onDelete,
             ),
           ],
         ),
       ),
     );
   }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+      appId: '1:270770596013:android:7c852f3bcef0bf4f183e24',
+      apiKey: 'AIzaSyCSIZkSC1toY1hJZpjTf1M7Gv985cO_TvM',
+      projectId: 'tracker-1d06d',
+      messagingSenderId: '270770596013',
+      // Add other necessary options
+    ),
+
+  );
+  runApp(BudgetTrackerApp());
 }
